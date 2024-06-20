d98f5be710a8fea05ad8299106d5b84fd9e95b25
ryanchou-dev <ryanchou111@gmail.com>
2024-06-19 22:12:08 -0700

    update: nextjs to 14.2

--- src/app/page.js
+++ src/app/page.js
@@ -126,7 +126,7 @@ export default function Home() {
 											</li>
 										</ol>
 									</div>
-									<p className="font-medium p-6 bg-green-800/20 rounded-2xl">
+									<div className="font-medium p-6 bg-green-800/20 rounded-2xl">
 										<div className=" text-black font-bold">Correlation</div>
 										<div className="text-gray-900">
 
@@ -135,8 +135,7 @@ export default function Home() {
 											<br />
 											Coupled with additional features, this website addresses this prompt effectively.
 										</div>
-
-									</p>
+									</div>
 								</div>
 
 
